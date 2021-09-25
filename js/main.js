699c4fdefccb95db95453a21cac16d6c7bd2f0a0
seyti67 <74255469+seyti67@users.noreply.github.com>
2021-09-25 19:32:44 +0200

    examples implemented

--- js/main.js
+++ js/main.js
@@ -1,4 +1,37 @@
+examples = [
+	{
+		name: 'exp(ix)',
+		func: 'e.pow(0,x)',
+		latex: 'e^{ix}'
+	},
+	{
+		name: 'exp(iπx/2)',
+		func: 'e.pow(0,pi*x/2)',
+		latex: 'e^{ix}'
+	},
+	{
+		name: 'i^x',
+		func: 'i.pow(x)',
+		latex: 'i^x'
+	},
+	{
+		name: 'Re(exp(ix))² + Im(exp(ix))²',
+		func: 'new Complex(Math.pow(Re(e.pow(0,x)),2)+Math.pow(Im(e.pow(0,x)),2))',
+		latex: 'Re(e^{ix})^2 + Im(e^{ix})^2'
+	}
+]
+let examplesStr = '';
+examples.forEach((example, index) => {
+	examplesStr += `<option value="${index}">${example.name}</option>`;
+});
+document.getElementById('example-list').innerHTML = examplesStr;
 
+function change_example(index) {
+	document.getElementById("func-string").value = examples[index].func;
+	document.getElementById('latex').innerHTML = '\\(z='+examples[index].latex+'\\)';
+	MathJax.typeset();
+	update_settings();
+}
 
 let funcString = "e.pow(0, x)";
 const e = new Complex(2.718281828459045);
@@ -25,17 +58,16 @@ function animate(timestamp) {
 }
 
 ctrlbtn = document.getElementById('control-button');
-let lastPause;
+let lastPause = new Date().getTime();
 function toggleAnimation() {
 	paused = !paused;
 	if(paused) {
 		ctrlbtn.className = "";
 		lastPause = new Date().getTime();
 	} else {
-		if(lastPause) {
-			let afkTimestamp = new Date().getTime() - lastPause;
-			xValue -= afkTimestamp*variation;
-		}
+		let afkTimestamp = new Date().getTime() - lastPause;
+		xValue -= afkTimestamp*variation;
+
 		ctrlbtn.className = "playing";
 		requestAnimationFrame(animate);
 	}
